33d0ed2a0d0df82223bb1016ac53c341d6bdf295
Al Slater <al.slater@scluk.com>
2018-08-14 08:41:45 +0000

    Git version bump to 2.18.0

--- devel/git-base/options.mk
+++ devel/git-base/options.mk
@@ -1,28 +1,13 @@
 # $NetBSD: options.mk,v 1.4 2016/04/11 04:22:33 dbj Exp $
 #
 PKG_OPTIONS_VAR=	PKG_OPTIONS.git
-PKG_SUPPORTED_OPTIONS=	python apple-common-crypto
+PKG_SUPPORTED_OPTIONS=	apple-common-crypto
 # python is not suggested because upstream's INSTALL does not list python
 # as a dependency and because all it does is install a python module,
 # which does not seem worth the dependency for everyone else.
 PKG_SUGGESTED_OPTIONS=
 
 .include "../../mk/bsd.options.mk"
-
-PLIST_VARS+=		python
-
-.if !empty(PKG_OPTIONS:Mpython)
-PY_PATCHPLIST=	yes
-CONFIGURE_ARGS+=	--with-python=${PYTHONBIN}
-PLIST.python=		yes
-# not executable
-CHECK_INTERPRETER_SKIP+= ${PYSITELIB}/git_remote_helpers/*.py
-CHECK_INTERPRETER_SKIP+= ${PYSITELIB}/git_remote_helpers/git/*.py
-.include "../../lang/python/application.mk"
-.include "../../lang/python/extension.mk"
-.else
-CONFIGURE_ARGS+=	--without-python
-.endif
 
 .if !empty(PKG_OPTIONS:Mapple-common-crypto)
 .  if !empty(MACHINE_PLATFORM:MDarwin-[0-8].*-*)
