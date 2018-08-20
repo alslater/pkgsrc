6db94cb507569f2a84b1c8772aa69ad9edf617d6
Al Slater <al.slater@scluk.com>
2018-08-20 07:36:36 +0000

    Coreutils version bump to 8.30

--- sysutils/coreutils/patches/patch-lib-fflush.c
+++ sysutils/coreutils/patches/patch-lib-fflush.c
@@ -1,11 +1,11 @@
 $NetBSD: patch-lib-fflush.c,v 1.3 2016/01/31 09:20:51 ryoon Exp $
 
---- lib/fflush.c.orig	2016-01-01 13:45:55.000000000 +0000
+--- lib/fflush.c.orig	2018-05-14 04:20:31.000000000 +0000
 +++ lib/fflush.c
-@@ -99,7 +99,7 @@ update_fpos_cache (FILE *fp _GL_UNUSED_P
+@@ -101,7 +101,7 @@ update_fpos_cache (FILE *fp _GL_UNUSED_P
  {
  #  if defined __sferror || defined __DragonFly__ || defined __ANDROID__
-   /* FreeBSD, NetBSD, OpenBSD, DragonFly, Mac OS X, Cygwin, Android */
+   /* FreeBSD, NetBSD, OpenBSD, DragonFly, Mac OS X, Cygwin, Minix 3, Android */
 -#   if defined __CYGWIN__
 +#   if defined(__CYGWIN__) || defined(__NetBSD__)
    /* fp_->_offset is typed as an integer.  */
---
+++ sysutils/coreutils/patches/patch-lib_sys-limits.h
@@ -0,0 +1,15 @@
+$NetBSD$
+
+--- lib/sys-limits.h.orig	2018-06-24 04:56:28.000000000 +0000
++++ lib/sys-limits.h
+@@ -15,8 +15,8 @@
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, see <https://www.gnu.org/licenses/>.  */
+ 
+-#ifndef _SYS_LIMITS_H
+-#define _SYS_LIMITS_H
++#ifndef _GNU_SYS_LIMITS_H
++#define _GNU_SYS_LIMITS_H
+ 
+ #include <limits.h>
+ 
