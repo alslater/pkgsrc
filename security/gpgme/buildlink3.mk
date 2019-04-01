8d2a12a7581fbb881fab831789f885cf2c4ec8bf
Al Slater <al.slater@scluk.com>
2019-04-01 08:06:18 +0000

    gpgme version bump to 1.12.0

--- security/gpgme/buildlink3.mk
+++ security/gpgme/buildlink3.mk
@@ -6,21 +6,17 @@ BUILDLINK_TREE+=	gpgme
 GPGME_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gpgme+=	gpgme>=0.9.0
-BUILDLINK_ABI_DEPENDS.gpgme+=	gpgme>=1.3.0nb1
+BUILDLINK_ABI_DEPENDS.gpgme+=	gpgme>=1.8.1nb1
 BUILDLINK_PKGSRCDIR.gpgme?=	../../security/gpgme
 
-.include "../../devel/librfuncs/buildlink3.mk"
 .include "../../security/libassuan2/buildlink3.mk"
 .include "../../security/libgpg-error/buildlink3.mk"
 
 pkgbase:=		gpgme
 .include "../../mk/pkg-build-options.mk"
 
-.if !empty(PKG_BUILD_OPTIONS.gpgme:Mgnupg21)
-DEPENDS+=		gnupg21>=2.1:../../security/gnupg21
-GPGME_GPG=		${PREFIX}/bin/gpg2
-.elif !empty(PKG_BUILD_OPTIONS.gpgme:Mgnupg2)
-DEPENDS+=		gnupg2>=2.0<2.1:../../security/gnupg2
+.if !empty(PKG_BUILD_OPTIONS.gpgme:Mgnupg2)
+DEPENDS+=		gnupg2>=2.2.0:../../security/gnupg2
 GPGME_GPG=		${PREFIX}/bin/gpg2
 .else
 DEPENDS+=		gnupg>=1.4.2:../../security/gnupg
--- security/gpgme/options.mk
+++ security/gpgme/options.mk
@@ -2,23 +2,17 @@
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.gpgme
 PKG_OPTIONS_REQUIRED_GROUPS=	gnupg
-PKG_OPTIONS_GROUP.gnupg=	gnupg1 gnupg2 gnupg21
+PKG_OPTIONS_GROUP.gnupg=	gnupg1 gnupg2 
 PKG_SUGGESTED_OPTIONS=		gnupg2
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mgnupg21)
-DEPENDS+=		gnupg21>=2.1:../../security/gnupg21
-CONFIGURE_ARGS+=	--with-gpgsm=${LOCALBASE}/bin/gpgsm
-CONFIGURE_ARGS+=	--with-gpg=${LOCALBASE}/bin/gpg2
-REPLACE_SH+=		tests/gpg/pinentry
-.elif !empty(PKG_OPTIONS:Mgnupg2)
-DEPENDS+=		gnupg2>=2.0<2.1:../../security/gnupg2
-CONFIGURE_ARGS+=	--with-gpgsm=${LOCALBASE}/bin/gpgsm
-CONFIGURE_ARGS+=	--with-gpg=${LOCALBASE}/bin/gpg2
-REPLACE_SH+=		tests/gpg/pinentry
+.if !empty(PKG_OPTIONS:Mgnupg2)
+DEPENDS+=       gnupg2>=2.2.0:../../security/gnupg2
+REPLACE_SH+=        tests/gpg/pinentry
+GPG_DEFAULT=        ${PREFIX}/bin/gpg2
+MAKE_FLAGS+=        GPG=${GPG_DEFAULT}
 .else
 DEPENDS+=		gnupg>=1.4.2:../../security/gnupg
-CONFIGURE_ARGS+=	--without-gpgconf
-CONFIGURE_ARGS+=	--without-gpgsm
+CONFIGURE_ARGS+=	--disable-gpgsm-test   # XXX: needs gpgsm
 .endif
