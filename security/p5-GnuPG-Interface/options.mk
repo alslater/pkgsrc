e28616a5e9dad41f2133aa6b17d7bbf5bb9968bd
Al Slater <al.slater@scluk.com>
2019-04-01 08:13:32 +0000

    p5-GnuPG-Interface allow use of gnupg2

---
+++ security/p5-GnuPG-Interface/options.mk
@@ -0,0 +1,17 @@
+# $NetBSD: options.mk,v 1.5 2018/01/09 10:22:47 wiz Exp $
+
+PKG_OPTIONS_VAR=                PKG_OPTIONS.p5_GnuPG_Interface
+PKG_OPTIONS_REQUIRED_GROUPS=    gnupg
+PKG_OPTIONS_GROUP.gnupg=        gnupg1 # gnupg2
+PKG_SUGGESTED_OPTIONS=          gnupg1
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mgnupg2)
+# patch available at
+# https://github.com/bestpractical/gnupg-interface/pull/1
+DEPENDS+=		gnupg2-[0-9]*:../../security/gnupg2
+.else
+DEPENDS+=		gnupg>=1.4.2:../../security/gnupg
+.endif
+
