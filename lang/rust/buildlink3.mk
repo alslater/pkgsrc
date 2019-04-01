ff05557841ccd92364d177f84745caf30292ef0e
Al Slater <al.slater@scluk.com>
2019-04-01 05:58:29 +0000

    rust version bump to 1.32.0

--- lang/rust/buildlink3.mk
+++ lang/rust/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2016/09/06 10:36:49 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2017/09/30 04:42:43 ryoon Exp $
 
 BUILDLINK_TREE+=	rust
 
 .if !defined(RUST_BUILDLINK3_MK)
 RUST_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.rust+=	rust>=0.11.0
+BUILDLINK_API_DEPENDS.rust+=	rust>=1.20.0
 BUILDLINK_PKGSRCDIR.rust?=	../../lang/rust
 
 BUILDLINK_PASSTHRU_DIRS+=	${PREFIX}/lib/rustlib
