a323f0dd185f2a5e5d798feb73a2d7e5ce1ae7e7
Al Slater <al.slater@scluk.com>
2019-09-19 11:00:39 +0000

    cmake version bump to 3.14.0

--- devel/cmake/patches/patch-Modules_FindCurses.cmake
+++ devel/cmake/patches/patch-Modules_FindCurses.cmake
@@ -1,4 +1,8 @@
-$NetBSD: patch-Modules_FindCurses.cmake,v 1.1.2.2 2017/01/13 19:33:46 bsiegert Exp $
+$NetBSD: patch-Modules_FindCurses.cmake,v 1.2 2019/01/04 17:47:12 triaxx Exp $
+
+Recognize libgnuform as valid implementation of a form library to match
+devel/ncurses. This is necessary due to cmake's insistance of scanning
+PREFIX/lib directly.
 
 --- Modules/FindCurses.cmake.orig	2017-01-05 22:24:12.384738412 +0000
 +++ Modules/FindCurses.cmake
