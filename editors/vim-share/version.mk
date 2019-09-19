be59bbfbaca0eba09727d1e681b27db0b5deae2e
Al Slater <al.slater@scluk.com>
2019-09-19 11:19:03 +0000

    vim version bump to 8.1

--- editors/vim-share/version.mk
+++ editors/vim-share/version.mk
@@ -1,5 +1,7 @@
 # $NetBSD: version.mk,v 1.99 2016/11/16 10:38:35 wiz Exp $
 
-VIM_VERSION=		8.0
-VIM_PATCHLEVEL=		0086
-VIM_SUBDIR=		vim80
+VIM_VERSION=		8.1
+VIM_PATCHLEVEL=		1523
+VIM_SUBDIR=		vim81
+
+PRINT_PLIST_AWK+=   { gsub(/${VIM_SUBDIR}/, "$${VIM_SUBDIR}"); print; next; }
