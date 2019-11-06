8ad580c22e0f176628bd21ea915d1dd5f48c7413
Al Slater <al.slater@scluk.com>
2019-11-06 09:55:44 +0000

    openjdk8 version bump to 1.8.222

--- lang/openjdk8/options.mk
+++ lang/openjdk8/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.3 2015/03/04 17:01:02 tnn Exp $
+# $NetBSD: options.mk,v 1.5 2019/09/22 14:42:47 tnn Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.openjdk8
 PKG_OPTIONS_OPTIONAL_GROUPS=	variant
@@ -46,8 +46,6 @@ DEPENDS+=		dejavu-ttf-[0-9]*:../../fonts/dejavu-ttf
 #DEPENDS+=		baekmuk-ttf-[0-9]*:../../fonts/baekmuk-ttf
 .include "../../x11/libXext/buildlink3.mk"
 .include "../../x11/libXi/buildlink3.mk"
-BUILDLINK_DEPMETHOD.libXp?=	build
-.include "../../x11/libXp/buildlink3.mk"
 BUILDLINK_DEPMETHOD.libXt?=	build
 .include "../../x11/libXt/buildlink3.mk"
 .include "../../x11/libXtst/buildlink3.mk"
@@ -75,7 +73,6 @@ remove-x11-classes:
 BUILD_DEBUG_LEVEL=	fastdebug
 CONFIGURE_ARGS+=	--enable-debug-symbols
 CONFIGURE_ARGS+=	--disable-zip-debug-info
-CONFIGURE_ARGS+=	--disable-precompiled-headers
 PLIST_SRC+=		PLIST.debug
 .else
 BUILD_DEBUG_LEVEL=	release
