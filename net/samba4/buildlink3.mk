ffbc2457e43daca0766c8006b9943d3d55ce0d36
Al Slater <al.slater@scluk.com>
2019-11-06 09:59:51 +0000

    samba4 version bump to 4.10.8

--- net/samba4/buildlink3.mk
+++ net/samba4/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2018/12/22 01:13:52 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2019/07/20 22:46:03 wiz Exp $
 
 BUILDLINK_TREE+=	samba
 
@@ -6,16 +6,18 @@ BUILDLINK_TREE+=	samba
 SAMBA_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.samba+=	samba>=4.9.4
+BUILDLINK_ABI_DEPENDS.samba?=	samba>=4.10.5nb2
 BUILDLINK_PKGSRCDIR.samba?=	../../net/samba4
 
 .include "../../archivers/libarchive/buildlink3.mk"
 .include "../../converters/libiconv/buildlink3.mk"
-.include "../../databases/lmdb/buildlink3.mk"
+.include "../../databases/ldb/buildlink3.mk"
 .include "../../devel/cmocka/buildlink3.mk"
 .include "../../devel/gettext-lib/buildlink3.mk"
 .include "../../devel/popt/buildlink3.mk"
 .include "../../devel/readline/buildlink3.mk"
 .include "../../devel/talloc/buildlink3.mk"
+.include "../../devel/tevent/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../security/gnutls/buildlink3.mk"
 .include "../../security/libgcrypt/buildlink3.mk"
