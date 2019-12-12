f26cf346b01071b50cd2583ef3418a4d450706d5
Al Slater <al.slater@scluk.com>
2019-12-12 06:40:17 +0000

    Import mercurial 5.2 from joyent trunk

---
+++ devel/py-mercurial/version.mk
@@ -0,0 +1,7 @@
+# $NetBSD: version.mk,v 1.7 2019/11/13 12:26:40 wiz Exp $
+
+VERSION=	5.2
+
+PYTHON_VERSIONS_ACCEPTED=	27 # 5.0 has "beta" support for python 3
+# see https://www.mercurial-scm.org/wiki/Python3
+# set HGPYTHON3=1 in the environment during install
