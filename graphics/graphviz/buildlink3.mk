e59dfa83e725893d0bac89b2d63d43c5ce0bae43
Al Slater <al.slater@scluk.com>
2019-12-14 19:12:35 +0000

    Import graphviz update from joyent trunk

--- graphics/graphviz/buildlink3.mk
+++ graphics/graphviz/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.23 2013/06/04 22:16:19 tron Exp $
+# $NetBSD: buildlink3.mk,v 1.26 2018/07/19 15:15:26 jaapb Exp $
 
 BUILDLINK_TREE+=	graphviz
 
@@ -6,8 +6,8 @@ BUILDLINK_TREE+=	graphviz
 GRAPHVIZ_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.graphviz+=	graphviz>=2.26.3
-BUILDLINK_ABI_DEPENDS.graphviz+=	graphviz>=2.26.3nb26
-BUILDLINK_PKGSRCDIR.graphviz?=	../../graphics/graphviz
+BUILDLINK_ABI_DEPENDS.graphviz+=	graphviz>=2.40.1nb22
+BUILDLINK_PKGSRCDIR.graphviz?=		../../graphics/graphviz
 
 # doxygen PLIST varies with pangocairo of PKG_OPTIONS
 pkgbase := graphviz
