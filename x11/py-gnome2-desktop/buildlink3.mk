5a458833cd528a287ab6a3cff1cc1d7bf2294dde
Al <al@coralnet.name>
2019-12-18 11:16:05 +0000

    Import py-gnome2-desktop update from joyent trunk

--- x11/py-gnome2-desktop/buildlink3.mk
+++ x11/py-gnome2-desktop/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.35 2016/08/03 10:22:24 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.45 2019/07/21 22:24:19 wiz Exp $
 
 BUILDLINK_TREE+=	py-gnome2-desktop
 
@@ -7,10 +7,10 @@ PY_GNOME2_DESKTOP_BUILDLINK3_MK:=
 
 .include "../../lang/python/pyversion.mk"
 
-BUILDLINK_API_DEPENDS.py-gnome2-desktop+=${PYPKGPREFIX}-gnome2-desktop>=2.16.0
-BUILDLINK_ABI_DEPENDS.py-gnome2-desktop?=${PYPKGPREFIX}-gnome2-desktop>=2.32.0nb36
-BUILDLINK_ABI_DEPENDS.py-gnome2-desktop?=	${PYPKGPREFIX}-gnome2-desktop>=2.32.0nb44
-BUILDLINK_PKGSRCDIR.py-gnome2-desktop?=	../../x11/py-gnome2-desktop
+BUILDLINK_API_DEPENDS.py-gnome2-desktop+=	${PYPKGPREFIX}-gnome2-desktop>=2.16.0
+BUILDLINK_ABI_DEPENDS.py-gnome2-desktop?=	${PYPKGPREFIX}-gnome2-desktop>=2.32.0nb67
+BUILDLINK_ABI_DEPENDS.py-gnome2-desktop?=	${PYPKGPREFIX}-gnome2-desktop>=2.32.0nb51
+BUILDLINK_PKGSRCDIR.py-gnome2-desktop?=		../../x11/py-gnome2-desktop
 
 .include "../../x11/py-gnome2/buildlink3.mk"
 .endif # PY_GNOME2_DESKTOP_BUILDLINK3_MK
