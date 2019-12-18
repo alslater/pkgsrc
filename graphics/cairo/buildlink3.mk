4f74912fbc220e38740cdb10c21b0c9082cc69fd
Al <al@coralnet.name>
2019-12-18 11:15:12 +0000

    Import cairo update from joyent trunk

--- graphics/cairo/buildlink3.mk
+++ graphics/cairo/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.54 2015/04/25 14:23:07 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.60 2019/09/04 13:19:33 nia Exp $
 
 BUILDLINK_TREE+=	cairo
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	cairo
 CAIRO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.cairo+=	cairo>=1.0.0nb2
-BUILDLINK_ABI_DEPENDS.cairo+=	cairo>=1.14.2nb1
+BUILDLINK_ABI_DEPENDS.cairo+=	cairo>=1.16.0
 BUILDLINK_PKGSRCDIR.cairo?=	../../graphics/cairo
 
 BUILDLINK_API_DEPENDS.Xrender+=	Xrender>=0.8
@@ -21,7 +21,7 @@ pkgbase := cairo
 .endif
 
 .if !empty(PKG_BUILD_OPTIONS.cairo:Mx11)
-.include "../../graphics/MesaLib/buildlink3.mk"
+.include "../../x11/libXext/buildlink3.mk"
 .include "../../x11/libXrender/buildlink3.mk"
 .endif
 
