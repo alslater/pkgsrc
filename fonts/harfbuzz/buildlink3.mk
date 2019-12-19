d16d98e5604fddca3be17228061fad3f1650b178
Al <al@coralnet.name>
2019-12-19 10:27:04 +0000

    Import harfbuzz update from joyent trunk

--- fonts/harfbuzz/buildlink3.mk
+++ fonts/harfbuzz/buildlink3.mk
@@ -1,16 +1,21 @@
-# $NetBSD: buildlink3.mk,v 1.6 2013/08/31 22:20:24 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2019/06/08 19:53:26 leot Exp $
 
 BUILDLINK_TREE+=	harfbuzz
 
 .if !defined(HARFBUZZ_BUILDLINK3_MK)
 HARFBUZZ_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.harfbuzz+=	harfbuzz>=0.9.4
-BUILDLINK_ABI_DEPENDS.harfbuzz+=	harfbuzz>=0.9.17nb2
+.include "../../graphics/librsvg/available.mk"
+
+BUILDLINK_API_DEPENDS.harfbuzz+=	harfbuzz>=2.1.1
+BUILDLINK_ABI_DEPENDS.harfbuzz+=	harfbuzz>=2.1.1
 BUILDLINK_PKGSRCDIR.harfbuzz?=		../../fonts/harfbuzz
 
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../graphics/freetype2/buildlink3.mk"
+.if ${LIBRSVG_TYPE} == "rust"
+.include "../../graphics/graphite2/buildlink3.mk"
+.endif
 .endif	# HARFBUZZ_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-harfbuzz
