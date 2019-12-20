87873ce6e8858c165d59921ef836007502da08c1
Al <al@coralnet.name>
2019-12-20 09:42:46 +0000

    Import libunwind update from joyent trunk

--- lang/libunwind/buildlink3.mk
+++ lang/libunwind/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2016/03/28 12:56:03 kamil Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2019/10/19 13:59:06 adam Exp $
 
 BUILDLINK_TREE+=	libunwind
 
 .if !defined(LIBUNWIND_BUILDLINK3_MK)
 LIBUNWIND_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libunwind+=	libunwind>=3.8.0
-BUILDLINK_PKGSRCDIR.libunwind?=	../../lang/libunwind
+BUILDLINK_API_DEPENDS.libunwind+=	libunwind>=9.0.0
+BUILDLINK_PKGSRCDIR.libunwind?=		../../lang/libunwind
 
 .include "../../lang/llvm/buildlink3.mk"
 .endif	# LIBUNWIND_BUILDLINK3_MK
