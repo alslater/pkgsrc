65afc1e8d940e38a757aae4cfe2fc562711b82fe
Al <al@coralnet.name>
2019-12-21 07:27:42 +0000

    Import vala update from joyent trunk

--- lang/vala/buildlink3.mk
+++ lang/vala/buildlink3.mk
@@ -1,20 +1,22 @@
-# $NetBSD: buildlink3.mk,v 1.14 2016/04/10 18:49:42 prlw1 Exp $
+# $NetBSD: buildlink3.mk,v 1.23 2019/11/06 14:13:45 wiz Exp $
 
 BUILDLINK_TREE+=	vala
 
 .if !defined(VALA_BUILDLINK3_MK)
 VALA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.vala+=	vala>=0.32
+BUILDLINK_API_DEPENDS.vala+=	vala>=0.46
 BUILDLINK_PKGSRCDIR.vala?=	../../lang/vala
 
-VALAC=${LOCALBASE}/bin/valac-0.32
-VAPIGEN=${LOCALBASE}/bin/vapigen-0.32
+VALAC=		${PREFIX}/bin/valac-0.46
+VAPIGEN=	${PREFIX}/bin/vapigen-0.46
 .if defined(GNU_CONFIGURE)
 CONFIGURE_ENV+=	VALAC=${VALAC} VAPIGEN=${VAPIGEN}
 .endif
 
 .include "../../devel/glib2/buildlink3.mk"
+.include "../../devel/libltdl/buildlink3.mk"
+.include "../../graphics/graphviz/buildlink3.mk"
 .endif	# VALA_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-vala
