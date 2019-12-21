a65326dcee9cd8200cdc4bb4d1f5d8cdd0caddc8
Al <al@coralnet.name>
2019-12-21 07:27:28 +0000

    Import libsoup update from joyent trunk

--- net/libsoup/buildlink3.mk
+++ net/libsoup/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.22 2016/12/04 05:17:10 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.32 2019/10/11 16:38:50 kamil Exp $
 
 BUILDLINK_TREE+=	libsoup
 
@@ -6,16 +6,18 @@ BUILDLINK_TREE+=	libsoup
 LIBSOUP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libsoup+=	libsoup>=2.50.0
-BUILDLINK_ABI_DEPENDS.libsoup+=	libsoup>=2.54.1nb2
+BUILDLINK_ABI_DEPENDS.libsoup+=	libsoup>=2.64.2nb3
 
 BUILDLINK_PKGSRCDIR.libsoup?=	../../net/libsoup
 
 pkgbase := libsoup
 .include "../../mk/pkg-build-options.mk"
 
+.include "../../archivers/brotli/buildlink3.mk"
 .include "../../databases/sqlite3/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
+.include "../../www/libpsl/buildlink3.mk"
 
 .if !empty(PKG_BUILD_OPTIONS.libsoup:Mgssapi)
 .include "../../mk/krb5.buildlink3.mk"
