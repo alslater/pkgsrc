0499ecf119f16bc426dff86406323e85dfa9b23d
Al <al@coralnet.name>
2019-12-30 11:37:16 +0000

    Import more qt5 updates from joyent trunk

---
+++ x11/qt5-qtnetworkauth/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD: buildlink3.mk,v 1.6 2019/09/16 19:24:54 adam Exp $
+
+BUILDLINK_TREE+=	qt5-qtnetworkauth
+
+.if !defined(QT5_QTNETWORKAUTH_BUILDLINK3_MK)
+QT5_QTNETWORKAUTH_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.qt5-qtnetworkauth+=	qt5-qtnetworkauth>=5.11.2
+BUILDLINK_ABI_DEPENDS.qt5-qtnetworkauth+=	qt5-qtnetworkauth>=5.13.1
+BUILDLINK_PKGSRCDIR.qt5-qtnetworkauth?=		../../x11/qt5-qtnetworkauth
+
+BUILDLINK_INCDIRS.qt5-qtnetworkauth+=	qt5/include
+BUILDLINK_LIBDIRS.qt5-qtnetworkauth+=	qt5/lib
+BUILDLINK_LIBDIRS.qt5-qtnetworkauth+=	qt5/plugins
+
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.endif	# QT5_QTNETWORKAUTH_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-qt5-qtnetworkauth
---
+++ x11/qt5-qtquickcontrols2/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.10 2019/09/16 19:24:54 adam Exp $
+
+BUILDLINK_TREE+=	qt5-qtquickcontrols2
+
+.if !defined(QT5_QTQUICKCONTROLS2_BUILDLINK3_MK)
+QT5_QTQUICKCONTROLS2_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.qt5-qtquickcontrols2+=	qt5-qtquickcontrols2>=5.10.0
+BUILDLINK_ABI_DEPENDS.qt5-qtquickcontrols2?=	qt5-qtquickcontrols2>=5.13.1
+BUILDLINK_PKGSRCDIR.qt5-qtquickcontrols2?=	../../x11/qt5-qtquickcontrols2
+
+.include "../../x11/qt5-qtdeclarative/buildlink3.mk"
+.endif	# QT5_QTQUICKCONTROLS2_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-qt5-qtquickcontrols2
