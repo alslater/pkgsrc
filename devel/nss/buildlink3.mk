adc02ca959f89e01386ac9a5e763e37047ce5d5a
Al <al@coralnet.name>
2019-12-30 09:57:26 +0000

    Import nss update from joyent trunk

--- devel/nss/buildlink3.mk
+++ devel/nss/buildlink3.mk
@@ -1,15 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.20 2013/09/14 10:29:22 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.22 2019/05/05 22:47:27 ryoon Exp $
+
 BUILDLINK_TREE+=	nss
 
 .if !defined(NSS_BUILDLINK3_MK)
 NSS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.nss+=	nss>=3.14.0
-BUILDLINK_ABI_DEPENDS.nss+=	nss>=3.14.0
+BUILDLINK_API_DEPENDS.nss+=	nss>=3.43nb2
+BUILDLINK_ABI_DEPENDS.nss+=	nss>=3.43nb2
 BUILDLINK_PKGSRCDIR.nss?=	../../devel/nss
 
-BUILDLINK_LIBDIRS.nss+=		lib${LIBARCHSUFFIX}/nss
-BUILDLINK_RPATHDIRS.nss+=	lib${LIBARCHSUFFIX}/nss
+BUILDLINK_LIBDIRS.nss+=		lib/nss
+BUILDLINK_RPATHDIRS.nss+=	lib/nss
 
 BUILDLINK_INCDIRS.nss+=		include/nss/nss
 
