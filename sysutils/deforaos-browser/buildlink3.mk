0c1e33b2214b3ca23783d9c88250313d8840c902
Al <al@coralnet.name>
2019-12-30 09:49:04 +0000

    Import daforaos-* updates from joyent trunk

--- sysutils/deforaos-browser/buildlink3.mk
+++ sysutils/deforaos-browser/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.13 2016/08/03 10:22:18 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2019/07/21 22:24:11 wiz Exp $
 
 BUILDLINK_TREE+=	deforaos-browser
 
@@ -6,8 +6,8 @@ BUILDLINK_TREE+=	deforaos-browser
 DEFORAOS_BROWSER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.deforaos-browser+=	deforaos-browser>=0.5.0
-BUILDLINK_ABI_DEPENDS.deforaos-browser?=	deforaos-browser>=0.5.1nb1
-BUILDLINK_PKGSRCDIR.deforaos-browser?=	../../sysutils/deforaos-browser
+BUILDLINK_ABI_DEPENDS.deforaos-browser?=	deforaos-browser>=0.5.1nb8
+BUILDLINK_PKGSRCDIR.deforaos-browser?=		../../sysutils/deforaos-browser
 
 .include "../../x11/deforaos-libdesktop/buildlink3.mk"
 .endif	# DEFORAOS_BROWSER_BUILDLINK3_MK
