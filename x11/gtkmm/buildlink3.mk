134956141edb855900f019494567a632e6a35ed0
Al <al@coralnet.name>
2020-01-05 09:35:45 +0000

    Import gtkmm update from joyent trunk

--- x11/gtkmm/buildlink3.mk
+++ x11/gtkmm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.42 2016/08/03 10:22:22 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.49 2019/07/21 22:24:17 wiz Exp $
 
 BUILDLINK_TREE+=	gtkmm
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gtkmm
 GTKMM_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gtkmm+=	gtkmm>=2.22.0
-BUILDLINK_ABI_DEPENDS.gtkmm+=	gtkmm>=2.24.4nb8
+BUILDLINK_ABI_DEPENDS.gtkmm+=	gtkmm>=2.24.5nb8
 BUILDLINK_PKGSRCDIR.gtkmm?=	../../x11/gtkmm
 
 .include "../../devel/atkmm/buildlink3.mk"
