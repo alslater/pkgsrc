99f3f5f884bfc9ba96301c82710264135c99e70d
Al <al@coralnet.name>
2020-01-12 17:04:51 +0000

    Import tcl update from joyent 2019Q4

--- lang/tcl/buildlink3.mk
+++ lang/tcl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.29 2016/10/21 11:45:44 kamil Exp $
+# $NetBSD: buildlink3.mk,v 1.31 2018/12/09 18:33:55 adam Exp $
 
 BUILDLINK_TREE+=	tcl
 
@@ -19,11 +19,11 @@ BUILDLINK_TRANSFORM+=	l:tcl8.6:tcl86
 
 _TOOLS_USE_PKGSRC.tclsh=	yes
 
-TCLCONFIG_SH?=		${BUILDLINK_PREFIX.tcl}/lib${LIBARCHSUFFIX}/tclConfig.sh
+TCLCONFIG_SH?=		${BUILDLINK_PREFIX.tcl}/lib/tclConfig.sh
 TCLSH=			${BUILDLINK_PREFIX.tcl}/bin/tclsh
 # Bundled library
-ITCL_VERSION=		4.0.5
-TDBC_VERSION=		1.0.4
+ITCL_VERSION=		4.1.2
+TDBC_VERSION=		1.1.0
 
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../mk/dlopen.buildlink3.mk"
