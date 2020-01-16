6d4b80e1239ec441c843cddaca383ed317a63a89
Al Slater <al.slater@scluk.com>
2020-01-16 06:50:11 +0000

    Import jq update from joyent 2019Q4

--- devel/jq/patches/patch-builtin.c
+++ devel/jq/patches/patch-builtin.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-configure.ac,v 1.4 2018/11/03 12:47:16 leot Exp $
+$NetBSD: patch-builtin.c,v 1.2 2018/11/03 12:47:16 leot Exp $
 
 Defining _GNU_SOURCE, _BSD_SOURCE etc. in C sources is problematic,
 because the result of the configure command may be inconsistent with it.
@@ -11,13 +11,18 @@ Part of pull request 1458, commit id `df9a0963f8fa6fca773b059dce22c598152f3edb':
 
 Also shared via PR pkg/52460.
 
---- configure.ac.orig	2018-11-02 14:52:43.000000000 +0000
-+++ configure.ac
-@@ -11,6 +11,7 @@ AC_PREREQ([2.64])
- AC_CONFIG_AUX_DIR([config])
- AM_INIT_AUTOMAKE([1.11.2 subdir-objects parallel-tests foreign -Wall])
- AM_SILENT_RULES([yes])
-+AC_USE_SYSTEM_EXTENSIONS
- AM_PROG_AR
- AM_MAINTAINER_MODE([enable])
- AC_PROG_CC
+--- src/builtin.c.orig	2018-11-02 01:49:29.000000000 +0000
++++ src/builtin.c
+@@ -1,12 +1,3 @@
+-#define _BSD_SOURCE
+-#define _GNU_SOURCE
+-#ifndef __sun__
+-# define _XOPEN_SOURCE
+-# define _XOPEN_SOURCE_EXTENDED 1
+-#else
+-# define _XPG6
+-# define __EXTENSIONS__
+-#endif
+ #include <sys/time.h>
+ #include <stdlib.h>
+ #include <stddef.h>
