fa930070ce5f6dde0cca1235c2c6d78775670b97
Al Slater <al.slater@scluk.com>
2020-01-23 12:46:49 +0000

    Replace omniORB 4.1 with 4.2

--- net/py-omniORBpy/patches/patch-modules_pyFixed.cc
+++ net/py-omniORBpy/patches/patch-modules_pyFixed.cc
@@ -1,26 +1,26 @@
 $NetBSD: patch-modules_pyFixed.cc,v 1.1 2015/12/25 20:09:28 joerg Exp $
 
---- modules/pyFixed.cc.orig	2015-12-25 19:52:09.000000000 +0000
+--- modules/pyFixed.cc.orig	2017-02-20 22:45:59.000000000 +0000
 +++ modules/pyFixed.cc
-@@ -99,7 +99,7 @@ omniPy::newFixedObject(PyObject* self, P
- 	return omniPy::newFixedObject(f);
+@@ -70,7 +70,7 @@ omniPy::newFixedObject(PyObject* self, P
        }
+ #if (PY_VERSION_HEX <= 0x03000000)
        else if (PyInt_Check(pyv)) {
 -	long l = PyInt_AsLong(pyv);
 +	CORBA::LongLong l = PyInt_AsLong(pyv);
  	CORBA::Fixed f(l);
  	return omniPy::newFixedObject(f);
        }
-@@ -138,7 +138,7 @@ omniPy::newFixedObject(PyObject* self, P
- 	// an integer value, then hack the scale to be correct.
+@@ -112,7 +112,7 @@ omniPy::newFixedObject(PyObject* self, P
  
+ #if (PY_VERSION_HEX <= 0x03000000)
  	if (PyInt_Check(pyv)) {
 -	  long l = PyInt_AsLong(pyv);
 +	  CORBA::LongLong l = PyInt_AsLong(pyv);
  	  CORBA::Fixed f(l);
  	  f.PR_changeScale(scale);
  	  f.PR_setLimits(digits, scale);
-@@ -368,7 +368,7 @@ extern "C" {
+@@ -366,7 +366,7 @@ extern "C" {
    fixed_coerce(PyObject** pv, PyObject** pw)
    {
      if (PyInt_Check(*pw)) {
