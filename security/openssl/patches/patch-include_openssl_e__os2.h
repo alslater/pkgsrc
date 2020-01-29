53d2ca5c915d9864aa6bd4b1d657908a773bc61c
Al Slater <al.slater@essiell.com>
2020-01-29 07:47:33 +0000

    openssl - Stop use of _Noreturn when compiled with c++17

---
+++ security/openssl/patches/patch-include_openssl_e__os2.h
@@ -0,0 +1,13 @@
+$NetBSD$
+
+--- include/openssl/e_os2.h.orig	2019-09-10 13:13:07.000000000 +0000
++++ include/openssl/e_os2.h
+@@ -279,7 +279,7 @@ typedef unsigned __int64 uint64_t;
+ #  define ossl_inline inline
+ # endif
+ 
+-# if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L
++# if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L && !defined(__cplusplus)
+ #  define ossl_noreturn _Noreturn
+ # elif defined(__GNUC__) && __GNUC__ >= 2
+ #  define ossl_noreturn __attribute__((noreturn))
