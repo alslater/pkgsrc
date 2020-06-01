265fd3b7805362a13e1877975125854628d69aaa
Al Slater <al.slater@scluk.com>
2020-06-01 07:46:08 +0000

    python37 - version bump to 3.7.6

--- lang/python37/dist.mk
+++ lang/python37/dist.mk
@@ -1,6 +1,6 @@
 # $NetBSD: dist.mk,v 1.6 2019/10/16 07:04:04 adam Exp $
 
-PY_DISTVERSION=	3.7.5
+PY_DISTVERSION=	3.7.7
 DISTNAME=	Python-${PY_DISTVERSION}
 EXTRACT_SUFX=	.tar.xz
 DISTINFO_FILE=	${.CURDIR}/../../lang/python37/distinfo
