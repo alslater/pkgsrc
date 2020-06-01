9fc63ffd83ab1539419d9ed8368349dbd958782f
Al Slater <al.slater@scluk.com>
2020-06-01 06:35:43 +0000

    python27 - version bump to 2.7.18

--- lang/python27/dist.mk
+++ lang/python27/dist.mk
@@ -1,6 +1,6 @@
 # $NetBSD: dist.mk,v 1.17 2019/10/21 09:40:35 adam Exp $
 
-PY_DISTVERSION=	2.7.17
+PY_DISTVERSION=	2.7.18
 DISTNAME=	Python-${PY_DISTVERSION}
 EXTRACT_SUFX=	.tar.xz
 DISTINFO_FILE=	${.CURDIR}/../../lang/python27/distinfo
