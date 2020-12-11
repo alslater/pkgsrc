127cf398d27c0cbeec2c8941983d17a50c302e88
Al Slater <al.slater@scluk.com>
2020-12-11 11:15:23 +0000

    icu - import update from joyent/release/trunk

--- textproc/icu/buildlink3.mk
+++ textproc/icu/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.41 2020/06/02 08:34:35 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.43 2020/11/06 04:05:57 gutteridge Exp $
 
 BUILDLINK_TREE+=	icu
 
@@ -6,8 +6,10 @@ BUILDLINK_TREE+=	icu
 ICU_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.icu+=	icu>=3.4
-BUILDLINK_ABI_DEPENDS.icu+=	icu>=67.1
+BUILDLINK_ABI_DEPENDS.icu+=	icu>=68.1
 BUILDLINK_PKGSRCDIR.icu?=	../../textproc/icu
+
+GCC_REQD+=		4.9
 .endif # ICU_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-icu
