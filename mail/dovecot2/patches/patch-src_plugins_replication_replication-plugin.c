571c3facea40245c1abb77f5875bd53c326c4a37
Al Slater <al.slater@scluk.com>
2021-04-01 11:59:37 +0000

    dovecot2 - updated to 2.3.13

--- mail/dovecot2/patches/patch-src_plugins_replication_replication-plugin.c
+++ mail/dovecot2/patches/patch-src_plugins_replication_replication-plugin.c
@@ -3,16 +3,16 @@ $NetBSD$
 Revert dovecot commit 3a2098a987b81fe89b74a9a628e9ef164f053633
 to restore replication of deleted emails
 
---- src/plugins/replication/replication-plugin.c.orig	2018-09-21 07:14:56.000000000 +0000
+--- src/plugins/replication/replication-plugin.c.orig	2020-12-22 13:26:52.000000000 +0000
 +++ src/plugins/replication/replication-plugin.c
 @@ -255,8 +255,9 @@ replication_want_sync_changes(const stru
  {
  	/* Replication needs to be triggered on all the user-visible changes,
  	   but not e.g. due to writes to cache file. */
 -	return (changes->changes_mask &
--		~MAIL_INDEX_TRANSACTION_CHANGE_OTHERS) != 0;
-+	/* return (changes->changes_mask &
-+		~MAIL_INDEX_TRANSACTION_CHANGE_OTHERS) != 0; */
+-		ENUM_NEGATE(MAIL_INDEX_TRANSACTION_CHANGE_OTHERS)) != 0;
++/*	return (changes->changes_mask &
++		ENUM_NEGATE(MAIL_INDEX_TRANSACTION_CHANGE_OTHERS)) != 0; */
 +	return changes->changes_mask;
  }
  
