afcf70e73fef409bfae31b9284abd8d89ef423e4
Al Slater <al.slater@scluk.com>
2021-04-01 10:56:48 +0000

    python - updates to latest

--- lang/python36/dist.mk
+++ lang/python36/dist.mk
@@ -1,6 +1,6 @@
 # $NetBSD: dist.mk,v 1.10 2019/07/03 20:10:41 adam Exp $
 
-PY_DISTVERSION=	3.6.12
+PY_DISTVERSION=	3.6.13
 DISTNAME=	Python-${PY_DISTVERSION}
 EXTRACT_SUFX=	.tar.xz
 DISTINFO_FILE=	${.CURDIR}/../../lang/python36/distinfo
