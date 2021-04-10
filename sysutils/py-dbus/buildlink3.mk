629df58ac2ec1bb18ecaf95727a3d9d578b7d0d9
Al Slater <al.slater@essiell.com>
2021-04-10 14:51:24 +0100

    py-dbus - import update from joyent/release/trunk

--- sysutils/py-dbus/buildlink3.mk
+++ sysutils/py-dbus/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.16 2012/09/15 10:06:31 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2020/03/08 16:48:12 wiz Exp $
 
 BUILDLINK_TREE+=	py-dbus
 
@@ -8,12 +8,11 @@ PY_DBUS_BUILDLINK3_MK:=
 .include "../../lang/python/pyversion.mk"
 
 BUILDLINK_API_DEPENDS.py-dbus+=	${PYPKGPREFIX}-dbus>=0.23.4
-BUILDLINK_ABI_DEPENDS.py-dbus+=	${PYPKGPREFIX}-dbus>=1.1.1nb1
+BUILDLINK_ABI_DEPENDS.py-dbus+=	${PYPKGPREFIX}-dbus>=1.2.16nb1
 BUILDLINK_PKGSRCDIR.py-dbus?=	../../sysutils/py-dbus
 
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../sysutils/dbus/buildlink3.mk"
-.include "../../sysutils/dbus-glib/buildlink3.mk"
 .include "../../sysutils/dbus-python-common/buildlink3.mk"
 .endif # PY_DBUS_BUILDLINK3_MK
 
