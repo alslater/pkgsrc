28ac40989298ad717f95b94fdded9278cb8edc66
Al Slater <al.slater@scluk.com>
2022-03-25 12:06:53 +0000

    libxml2 - download from gnome.org, update to 2.9.13

--- textproc/libxml2/patches/patch-python_setup.py
+++ textproc/libxml2/patches/patch-python_setup.py
@@ -4,31 +4,17 @@ Trust pkgsrc to know which iconv to use.
 It will be either in the buildlink directory, or in the compiler's
 default search path.
 
---- python/setup.py.orig	2014-10-16 07:35:21.000000000 +0000
+--- python/setup.py.orig	2022-02-20 12:11:17.000000000 +0000
 +++ python/setup.py
-@@ -56,11 +56,7 @@ else:
- # - iconv.h
+@@ -66,11 +66,7 @@ else:
+ # - libxml2/libxml/tree.h
  # - libxslt/xsltconfig.h
  includes_dir = [
 -"/usr/include",
 -"/usr/local/include",
 -"/opt/include",
--os.path.join(ROOT,'include'),
+ os.path.join(ROOT,'include'),
 -HOME
-+os.path.join(ROOT,'include')
  ];
  
  xml_includes=""
-@@ -73,11 +69,7 @@ if xml_includes == "":
-     print("failed to find headers for libxml2: update includes_dir")
-     sys.exit(1)
- 
--iconv_includes=""
--for dir in includes_dir:
--    if not missing(dir + "/iconv.h"):
--        iconv_includes=dir
--        break;
-+iconv_includes=os.path.join(ROOT,'include')
- 
- if iconv_includes == "":
-     print("failed to find headers for libiconv: update includes_dir")
