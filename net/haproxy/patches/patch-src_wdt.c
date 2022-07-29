9b67297709711740281d439ecc403d4335898382
Al Slater <al.slater@scluk.com>
2022-07-29 15:59:42 +0000

    haproxy - update to 2.0.29

--- net/haproxy/patches/patch-src_wdt.c
+++ net/haproxy/patches/patch-src_wdt.c
@@ -3,9 +3,9 @@ $NetBSD$
 Backport https://github.com/haproxy/haproxy/commit/d6f196654 and
 https://github.com/haproxy/haproxy/commit/7259fa2b8
 
---- src/wdt.c.orig	2020-02-13 06:58:50.000000000 +0000
+--- src/wdt.c.orig	2022-05-13 15:43:21.000000000 +0000
 +++ src/wdt.c
-@@ -143,7 +143,8 @@ int init_wdt_per_thread()
+@@ -141,7 +141,8 @@ int init_wdt_per_thread()
  	sev.sigev_notify          = SIGEV_SIGNAL;
  	sev.sigev_signo           = WDTSIG;
  	sev.sigev_value.sival_int = tid;
@@ -15,12 +15,3 @@ https://github.com/haproxy/haproxy/commit/7259fa2b8
  		goto fail1;
  
  	if (!wdt_ping(tid))
-@@ -156,7 +157,7 @@ int init_wdt_per_thread()
-  fail1:
- 	ti->wd_timer = TIMER_INVALID;
- 	ha_warning("Failed to setup watchdog timer for thread %u, disabling lockup detection.\n", tid);
--	return 0;
-+	return 1;
- }
- 
- void deinit_wdt_per_thread()
