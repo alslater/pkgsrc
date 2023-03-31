36b8bb8088a735a002ddf980fe1ef89863ab92b0
Al Slater <al.slater@essiell.com>
2023-03-31 15:41:19 +0100

    llvm - import update from joyent/release/trunk

--- lang/llvm/patches/patch-cmake_config-ix.cmake
+++ lang/llvm/patches/patch-cmake_config-ix.cmake
@@ -1,15 +1,20 @@
-$NetBSD: patch-cmake_config-ix.cmake,v 1.4 2019/06/02 08:35:55 adam Exp $
+$NetBSD: patch-cmake_config-ix.cmake,v 1.7 2022/08/12 08:37:58 pin Exp $
 
 Do not generate invalid llvm-config in pkgsrc.
+Allow override of pthread library selection via PKGSRC_LLVM_PTHREADLIB.
 
---- cmake/config-ix.cmake.orig	2018-11-26 00:03:39.000000000 +0000
+--- cmake/config-ix.cmake.orig	2022-06-22 16:46:24.000000000 +0000
 +++ cmake/config-ix.cmake
-@@ -129,7 +129,7 @@ if(NOT LLVM_USE_SANITIZER MATCHES "Memor
-     endif()
-     if(LLVM_ENABLE_TERMINFO)
-       set(HAVE_TERMINFO 0)
--      foreach(library terminfo tinfo curses ncurses ncursesw)
-+      foreach(library terminfo curses ncurses ncursesw tinfo)
-         string(TOUPPER ${library} library_suffix)
-         check_library_exists(${library} setupterm "" HAVE_TERMINFO_${library_suffix})
-         if(HAVE_TERMINFO_${library_suffix})
+@@ -113,7 +113,11 @@ if(HAVE_LIBPTHREAD)
+   set(CMAKE_THREAD_PREFER_PTHREAD TRUE)
+   set(THREADS_HAVE_PTHREAD_ARG Off)
+   find_package(Threads REQUIRED)
+-  set(LLVM_PTHREAD_LIB ${CMAKE_THREAD_LIBS_INIT})
++  if(PKGSRC_LLVM_PTHREADLIB)
++    set(LLVM_PTHREAD_LIB ${PKGSRC_LLVM_PTHREADLIB})
++  else()
++    set(LLVM_PTHREAD_LIB ${CMAKE_THREAD_LIBS_INIT})
++  endif()
+ endif()
+ 
+ if(LLVM_ENABLE_ZLIB)
--- lang/llvm/patches/patch-cmake_modules_AddLLVM.cmake
+++ lang/llvm/patches/patch-cmake_modules_AddLLVM.cmake
@@ -1,36 +1,25 @@
-$NetBSD: patch-cmake_modules_AddLLVM.cmake,v 1.5 2019/10/19 13:52:40 adam Exp $
+$NetBSD: patch-cmake_modules_AddLLVM.cmake,v 1.8 2021/11/23 20:55:44 wiz Exp $
 
-Don't use non-portable -z discard-unused on SunOS.
 On Darwin, create symbolic links to shared libraries.
 On Darwin, use correct install-name for shared libraries.
 
---- cmake/modules/AddLLVM.cmake.orig	2019-07-12 14:40:18.000000000 +0000
+--- cmake/modules/AddLLVM.cmake.orig	2021-09-24 16:18:10.000000000 +0000
 +++ cmake/modules/AddLLVM.cmake
-@@ -222,9 +222,6 @@ function(add_link_opts target_name)
-         # ld64's implementation of -dead_strip breaks tools that use plugins.
-         set_property(TARGET ${target_name} APPEND_STRING PROPERTY
-                      LINK_FLAGS " -Wl,-dead_strip")
--      elseif(${CMAKE_SYSTEM_NAME} MATCHES "SunOS")
--        set_property(TARGET ${target_name} APPEND_STRING PROPERTY
--                     LINK_FLAGS " -Wl,-z -Wl,discard-unused=sections")
-       elseif(NOT WIN32 AND NOT LLVM_LINKER_IS_GOLD AND
-              NOT ${CMAKE_SYSTEM_NAME} MATCHES "OpenBSD|AIX")
-         # Object files are compiled with -ffunction-data-sections.
-@@ -536,7 +533,7 @@ function(llvm_add_library name)
+@@ -607,7 +607,7 @@ function(llvm_add_library name)
    endif()
  
-   if(ARG_SHARED AND UNIX)
+   if(ARG_SHARED)
 -    if(NOT APPLE AND ARG_SONAME)
 +    if(ARG_SONAME)
        get_target_property(output_name ${name} OUTPUT_NAME)
        if(${output_name} STREQUAL "output_name-NOTFOUND")
          set(output_name ${name})
-@@ -1727,7 +1724,7 @@ function(llvm_setup_rpath name)
+@@ -2118,7 +2118,7 @@ function(llvm_setup_rpath name)
    endif()
  
    if (APPLE)
 -    set(_install_name_dir INSTALL_NAME_DIR "@rpath")
 +    set(_install_name_dir INSTALL_NAME_DIR "${CMAKE_INSTALL_PREFIX}/lib")
-     set(_install_rpath "@loader_path/../lib" ${extra_libdir})
-   elseif(UNIX)
-     set(_install_rpath "\$ORIGIN/../lib${LLVM_LIBDIR_SUFFIX}" ${extra_libdir})
+     set(_install_rpath "@loader_path/../lib${LLVM_LIBDIR_SUFFIX}" ${extra_libdir})
+   elseif(${CMAKE_SYSTEM_NAME} MATCHES "AIX" AND BUILD_SHARED_LIBS)
+     # $ORIGIN is not interpreted at link time by aix ld.
