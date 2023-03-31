5bc17729c9154762e4ca124e42dc58abd040bbdc
Al Slater <al.slater@essiell.com>
2023-03-31 08:52:46 +0100

    erlang - imported update from joyent/release/trunk

--- lang/erlang/buildlink3.mk
+++ lang/erlang/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.14 2015/07/18 08:55:16 fhajny Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2022/06/28 11:34:10 wiz Exp $
 
 BUILDLINK_TREE+=	erlang
 
@@ -6,17 +6,17 @@ BUILDLINK_TREE+=	erlang
 ERLANG_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.erlang+=	erlang>=10.1
-BUILDLINK_ABI_DEPENDS.erlang+=	erlang>=18.0
+BUILDLINK_ABI_DEPENDS.erlang+=	erlang>=23.3.4nb2
 BUILDLINK_PKGSRCDIR.erlang?=	../../lang/erlang
 
 pkgbase := erlang
 .include "../../mk/pkg-build-options.mk"
 
-.if !empty(PKG_BUILD_OPTIONS.erlang:Miodbc)
+.if ${PKG_BUILD_OPTIONS.erlang:Miodbc}
 .  include "../../databases/iodbc/buildlink3.mk"
 .endif
 
-.if !empty(PKG_BUILD_OPTIONS.erlang:Munixodbc)
+.if ${PKG_BUILD_OPTIONS.erlang:Munixodbc}
 .  include "../../databases/unixodbc/buildlink3.mk"
 .endif
 
