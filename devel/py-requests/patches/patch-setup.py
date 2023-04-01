3e7596987b6282c871cc9bfdafe2ed02fc25540d
Al Slater <al.slater@essiell.com>
2023-04-01 12:44:16 +0100

    py-requests - imported update from joyent/release/trunk

--- devel/py-requests/patches/patch-setup.py
+++ devel/py-requests/patches/patch-setup.py
@@ -1,25 +1,15 @@
-$NetBSD: patch-setup.py,v 1.8 2019/05/17 08:34:38 adam Exp $
+$NetBSD: patch-setup.py,v 1.10 2022/07/10 20:09:56 wiz Exp $
 
-Allow newer versions of dependencies.
+Allow newer test dependencies.
 
---- setup.py.orig	2019-05-16 14:22:45.000000000 +0000
+--- setup.py.orig	2022-06-29 15:09:45.000000000 +0000
 +++ setup.py
-@@ -42,14 +42,14 @@ if sys.argv[-1] == 'publish':
- packages = ['requests']
- 
- requires = [
--    'chardet>=3.0.2,<3.1.0',
--    'idna>=2.5,<2.9',
--    'urllib3>=1.21.1,<1.26,!=1.25.0,!=1.25.1',
-+    'chardet>=3.0.2',
-+    'idna>=2.5',
-+    'urllib3>=1.21.1,!=1.25.0,!=1.25.1',
-     'certifi>=2017.4.17'
- 
+@@ -65,7 +65,7 @@ requires = [
+     "certifi>=2017.4.17",
  ]
  test_requirements = [
--    'pytest-httpbin==0.0.7',
-+    'pytest-httpbin>=0.0.7',
-     'pytest-cov',
-     'pytest-mock',
-     'pytest-xdist',
+-    "pytest-httpbin==0.0.7",
++    "pytest-httpbin>=0.0.7",
+     "pytest-cov",
+     "pytest-mock",
+     "pytest-xdist",
