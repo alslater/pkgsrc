7d9f45c3770a17d32c59f788a55961f0ac10bfdb
Al Slater <al.slater@scluk.com>
2023-09-01 09:42:46 +0000

    openssl - major update to 3.0.10

--- security/openssl/buildlink3.mk
+++ security/openssl/buildlink3.mk
@@ -7,8 +7,8 @@ OPENSSL_BUILDLINK3_MK:=
 
 .include "../../mk/bsd.fast.prefs.mk"
 
-BUILDLINK_API_DEPENDS.openssl+=	openssl>=1.1.1
-BUILDLINK_ABI_DEPENDS.openssl+=	openssl>=1.1.1dnb2
+BUILDLINK_API_DEPENDS.openssl+=	openssl>=3.0.0
+BUILDLINK_ABI_DEPENDS.openssl+=	openssl>=3.0.0
 BUILDLINK_PKGSRCDIR.openssl?=	../../security/openssl
 
 # Ensure that -lcrypt comes before -lcrypto when linking so that the
