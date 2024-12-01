071cc2e16dfc5a3ac2b9b202a272164bd5eae132
Al Slater <al.slater@scluk.com>
2024-12-01 14:21:30 +0000

    haproxy - update to 2.6.19

--- net/haproxy/patches/patch-include_haproxy_server-t.h
+++ net/haproxy/patches/patch-include_haproxy_server-t.h
@@ -2,14 +2,14 @@ $NetBSD: patch-include_haproxy_server-t.h,v 1.1 2021/12/09 10:36:07 jperkin Exp 
 
 Avoid 'struct queue' conflict.
 
---- include/haproxy/server-t.h.orig	2021-11-23 14:40:21.000000000 +0000
+--- include/haproxy/server-t.h.orig	2024-09-19 15:54:06.000000000 +0000
 +++ include/haproxy/server-t.h
-@@ -282,7 +282,7 @@ struct server {
+@@ -285,7 +285,7 @@ struct server {
  	unsigned int est_need_conns;            /* Estimate on the number of needed connections (max of curr and previous max_used) */
  	unsigned int next_takeover;             /* thread ID to try to steal connections from next time */
  
 -	struct queue queue;			/* pending connections */
 +	struct haqueue queue;			/* pending connections */
+ 	struct mt_list sess_conns;		/* list of private conns managed by a session on this server */
+ 	unsigned int dequeuing;                 /* non-zero = dequeuing in progress (atomic) */
  
- 	/* Element below are usd by LB algorithms and must be doable in
- 	 * parallel to other threads reusing connections above.
