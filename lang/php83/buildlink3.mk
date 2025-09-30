09ef29cfc86e19f388f4e3e6babcaab3ca3cf15a
Al Slater <al.slater@essiell.com>
2025-09-30 09:10:34 +0100

    php83 wip

---
+++ lang/php83/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.6 2025/04/17 21:51:22 wiz Exp $
+
+BUILDLINK_TREE+=	php
+
+.if !defined(PHP83_BUILDLINK3_MK)
+PHP83_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.php83+=	php${PHP_VER}>=8.3.0<8.4
+BUILDLINK_ABI_DEPENDS.php83+=	php83>=8.3.20nb1
+BUILDLINK_PKGSRCDIR.php83?=	../../lang/php83
+
+pkgbase := php83
+.include "../../mk/pkg-build-options.mk"
+.include "../../textproc/libxml2/buildlink3.mk"
+.endif # PHP83_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-php
