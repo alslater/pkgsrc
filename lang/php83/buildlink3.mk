f41bd0e555920c9ff3d00145d84c7dbd516ad9ed
Al Slater <al.slater@scluk.com>
2025-09-30 12:49:20 +0000

    php83 wip

--- lang/php83/buildlink3.mk
+++ lang/php83/buildlink3.mk
@@ -5,9 +5,9 @@ BUILDLINK_TREE+=	php
 .if !defined(PHP_BUILDLINK3_MK)
 PHP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.php83+=	php>=8.3.0<8.4.0
-BUILDLINK_ABI_DEPENDS.php83+=	php>=8.3.0
-BUILDLINK_PKGSRCDIR.php83?=	../../lang/php83
+BUILDLINK_API_DEPENDS.php+=	php>=8.3.0<8.4.0
+BUILDLINK_ABI_DEPENDS.php+=	php>=8.3.0
+BUILDLINK_PKGSRCDIR.php?=	../../lang/php83
 
 pkgbase := php
 .include "../../mk/pkg-build-options.mk"
