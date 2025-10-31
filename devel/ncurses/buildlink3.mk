128518ce89add6fbf71f246ec44bfa80d0de6868
Al Slater <al.slater@essiell.com>
2025-10-31 10:23:13 +0000

    ncurses - import update from joyent trunck

--- devel/ncurses/buildlink3.mk
+++ devel/ncurses/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.46 2015/08/17 17:10:31 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.51 2024/05/06 15:08:02 jperkin Exp $
 
 BUILDLINK_TREE+=	ncurses
 
@@ -6,11 +6,20 @@ BUILDLINK_TREE+=	ncurses
 NCURSES_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ncurses+=	ncurses>=5.3nb1
-BUILDLINK_ABI_DEPENDS.ncurses+=	ncurses>=6.0
+BUILDLINK_ABI_DEPENDS.ncurses+=	ncurses>=6.5
 BUILDLINK_PKGSRCDIR.ncurses?=	../../devel/ncurses
 
+BUILDLINK_INCDIRS.ncurses+=	include/ncurses
 BUILDLINK_LIBNAME.ncurses=	ncurses
 BUILDLINK_LDADD.ncurses?=	${BUILDLINK_LIBNAME.ncurses:S/^/-l/:S/^-l$//}
+
+# If a package specifies USE_CURSES=wide then we must enable the wide API in
+# the ncurses headers.  Using the NCURSES_WIDECHAR define is much less
+# intrusive than setting _XOPEN_SOURCE_EXTENDED or _XOPEN_SOURCE.
+#
+.if ${USE_CURSES:U:Mwide}
+BUILDLINK_CPPFLAGS.ncurses+=	-DNCURSES_WIDECHAR=1
+.endif
 
 # Many packages expect the ncurses headers and libraries to be usable as
 # <curses.h> and -lcurses and they often only look in include/ rather than
@@ -20,24 +29,16 @@ BUILDLINK_LDADD.ncurses?=	${BUILDLINK_LIBNAME.ncurses:S/^/-l/:S/^-l$//}
 BUILDLINK_TARGETS+=		buildlink-ncurses-curses-h
 BUILDLINK_TARGETS+=		buildlink-ncurses-ncurses-h
 BUILDLINK_TARGETS+=		buildlink-ncurses-term-h
-BUILDLINK_INCDIRS.ncurses+=	include/ncurses
+BUILDLINK_TARGETS+=		buildlink-ncursesw-pc
 
-# Many packages will prefer ncursesw over ncurses if its available (say as
-# a native library), so unless this file is being included by ncursesw
-# don't allow ncursesw to be used by causing linkage failure.
+# Some packages will only enable wide curses support if they specifically
+# find it in libncursesw, so redirect requests for it to libncurses.
 #
-.  include "../../mk/bsd.fast.prefs.mk"
-.  if !empty(PKGPATH:Mdevel/ncursesw)
 BUILDLINK_TRANSFORM+=		l:curses:${BUILDLINK_LIBNAME.ncurses}
-.  elif empty(BUILDLINK_TREE:Mncursesw)
-BUILDLINK_TRANSFORM+=		l:ncursesw:__nonexistent__
-BUILDLINK_TRANSFORM+=		l:curses:${BUILDLINK_LIBNAME.ncurses}
-.  else
-BUILDLINK_TRANSFORM+=		l:curses:${BUILDLINK_LIBNAME.ncursesw}
-.  endif
+BUILDLINK_TRANSFORM+=		l:ncursesw:${BUILDLINK_LIBNAME.ncurses}
 
 .PHONY: buildlink-ncurses-curses-h buildlink-ncurses-ncurses-h
-.PHONY: buildlink-ncurses-term-h
+.PHONY: buildlink-ncurses-term-h buildlink-ncursesw-pc
 buildlink-ncurses-curses-h:
 	${RUN}									\
 	src=${BUILDLINK_PREFIX.ncurses}"/include/ncurses/curses.h";		\
@@ -64,6 +65,16 @@ buildlink-ncurses-term-h:
 	dest=${BUILDLINK_DIR}"/include/term.h";				\
 	if ${TEST} ! -f "$$dest" -a -f "$$src"; then				\
 		${ECHO_BUILDLINK_MSG} "Linking ncurses/term.h -> term.h.";\
+		${MKDIR} `${DIRNAME} "$$dest"`;					\
+		${LN} -s "$$src" "$$dest";					\
+	fi
+
+buildlink-ncursesw-pc:
+	${RUN}									\
+	src=${BUILDLINK_PREFIX.ncurses}"/lib/pkgconfig/ncurses.pc";		\
+	dest=${BUILDLINK_DIR}"/lib/pkgconfig/ncursesw.pc";			\
+	if ${TEST} ! -f "$$dest" -a -f "$$src"; then				\
+		${ECHO_BUILDLINK_MSG} "Linking ncursesw.pc -> ncurses.pc.";\
 		${MKDIR} `${DIRNAME} "$$dest"`;					\
 		${LN} -s "$$src" "$$dest";					\
 	fi
