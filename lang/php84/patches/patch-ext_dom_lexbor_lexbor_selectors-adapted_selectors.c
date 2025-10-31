0d1cc5ef5959dd13441a4e5d34026542a01e60f8
Al Slater <al.slater@scluk.com>
2025-10-31 06:51:14 +0000

    October 2025 updates

---
+++ lang/php84/patches/patch-ext_dom_lexbor_lexbor_selectors-adapted_selectors.c
@@ -0,0 +1,13 @@
+$NetBSD$
+
+--- ext/dom/lexbor/lexbor/selectors-adapted/selectors.c.orig	2025-09-23 14:14:32.000000000 +0000
++++ ext/dom/lexbor/lexbor/selectors-adapted/selectors.c
+@@ -13,7 +13,7 @@
+ #include <Zend/zend_API.h>
+ #include <php.h>
+ 
+-#include "ext/dom/lexbor/lexbor/selectors-adapted/selectors.h"
++#include "lexbor/selectors-adapted/selectors.h"
+ #include "../../../namespace_compat.h"
+ #include "../../../domexception.h"
+ #include "../../../php_dom.h"
