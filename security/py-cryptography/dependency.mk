cfb439da7f76a080c047b824532a3e55e4fcc8e6
Al Slater <al.slater@essiell.com>
2025-11-02 08:31:33 +0000

    Import python updates from joyent trunk

---
+++ security/py-cryptography/dependency.mk
@@ -0,0 +1,40 @@
+# $NetBSD: dependency.mk,v 1.6 2024/08/04 09:42:36 kim Exp $
+#
+# Not for public use - use through versioned_dependencies.mk.
+#
+# User-settable variables:
+#
+# PYCRYPTOGRAPHY_TYPE
+#	Type of py-cryptography implementation used.
+#
+#	Possible values: c rust
+#	Default: rust on platforms were Rust is supported
+#
+
+.include "../../mk/bsd.fast.prefs.mk"
+
+.include "../../lang/rust/platform.mk"
+.if ${PLATFORM_SUPPORTS_RUST:tl} == "yes"
+PYCRYPTOGRAPHY_TYPE?=	rust
+.else
+PYCRYPTOGRAPHY_TYPE?=	c
+.endif
+
+.include "../../lang/python/pyversion.mk"
+.if ${PYCRYPTOGRAPHY_TYPE:tl} == "rust"
+PYCRYPTOGRAPHY_VERSION?=	cryptography>=43
+PYCRYPTOGRAPHY_DIR?=		security/py-cryptography
+.else
+PYCRYPTOGRAPHY_VERSION?=	cryptography>=0<3.4
+PYCRYPTOGRAPHY_DIR?=		security/py27-cryptography
+.endif
+
+.if "${type}" == ":build"
+TOOL_DEPENDS:=	${TOOL_DEPENDS} ${PYPKGPREFIX}-${PYCRYPTOGRAPHY_VERSION}:../../${PYCRYPTOGRAPHY_DIR}
+.elif "${type}" == ":test"
+TEST_DEPENDS:=	${TEST_DEPENDS} ${PYPKGPREFIX}-${PYCRYPTOGRAPHY_VERSION}:../../${PYCRYPTOGRAPHY_DIR}
+.elif "${type}" == ":tool"
+TOOL_DEPENDS:=	${TOOL_DEPENDS} ${PYPKGPREFIX}-${PYCRYPTOGRAPHY_VERSION}:../../${PYCRYPTOGRAPHY_DIR}
+.else
+DEPENDS:=	${DEPENDS} ${PYPKGPREFIX}-${PYCRYPTOGRAPHY_VERSION}:../../${PYCRYPTOGRAPHY_DIR}
+.endif
